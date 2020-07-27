afedf610432833fc1a11f71efc4fe8a5bbbe9981
Damian <Damian@BLINKIN-NETBOOK>
2020-07-27 20:36:34 +0200

    Starting Quiz - Ticket reservation

---
+++ Section11/QuizDK/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizDK
+{
+    abstract class Ticket
+    {
+        private string eventName;
+        private string personName;
+        private int seatNumber;
+        private int ticketCode;
+        private DateTime eventDate;
+
+        public Ticket(string name, string person, int seat, int code, DateTime date)
+        {
+            name = eventName;
+            person = personName;
+            seat = seatNumber;
+            code = ticketCode;
+            date = eventDate;
+        }
+
+        public abstract void Reservation();
+    }
+}
