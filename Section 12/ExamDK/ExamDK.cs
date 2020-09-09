eeccf5bda2f5038a149bc08e6ebbdb3f6363ecb7
Damian <Damian@BLINKIN-NETBOOK>
2020-09-09 22:04:08 +0200

    Passed the exam about Interfaces

---
+++ Section 12/ExamDK/ExamDK.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Section12.ExamDK;
+
+namespace Section12.Exam
+{
+    [TestClass]
+    [TestCategory("InterfaceExam")]
+    public class ExamDK
+    {
+        //In this exam, you must make the 2 tests below pass without changing a 
+        //single line of code in the tests.
+        //You basically need to fix all of the compilation errors and make the tests pass, 
+        //without touching the tests.
+        [TestMethod]
+        public void Test_Page1()
+        {
+            IPageObjectDK pageObject1 = new PageObjectOneDK();
+            Assert.AreEqual(pageObject1.PageName, "Page Name 1");
+        }
+        [TestMethod]
+        public void Test_Page2()
+        {
+            IPageObjectDK pageObject2 = new PageObjectTwoDK();
+            Assert.AreEqual(pageObject2.PageName, "Page Name 2");
+        }
+    }
+}
---
+++ Section 12/ExamDK/PageObjectTwoDK.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section12.ExamDK
+{
+    class PageObjectTwoDK : IPageObjectDK
+    {
+        public string PageName
+        {
+            get
+            {
+                return "Page Name 2";
+            }
+            set
+            {
+
+            }
+        }
+
+    }
+}
